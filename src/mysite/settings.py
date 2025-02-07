274ca0eee68449f8e028b69057d1760d7dc6cd10
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-02-07 20:20:55 +0545

    added vercel settings

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'personal',
     
     #djano apps
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
