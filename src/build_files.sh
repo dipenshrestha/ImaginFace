9b780ac380c4da3e354a55663a06d5a5eacfb758
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-02-07 20:38:53 +0545

    added RENDER settings

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,2 +1,13 @@
-python3.12.4 -m pip install -r requirements.txt
-python3.12.4 manage.py collectstatic --noinput --clear
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
